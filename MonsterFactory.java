ef42b5852db80cacdb743fbdf0ad472d49708347
Blake Impecoven <bimpecoven@Blakes-MacBook-Pro.local>
2017-03-16 13:31:23 -0700

    Added 2 new monsters, updated factory and added their own attacks

--- MonsterFactory.java
+++ MonsterFactory.java
@@ -6,21 +6,79 @@
 public class MonsterFactory {
     public Monster createCharacter(int type) {
         Monster monster;
+        String name;
+        int hitPoints, attackSpeed, damageMin, damageMax, minHeal, maxHeal;
+        double chanceToHit, chanceToHeal;
 
         switch(type) {
-            case 1:     monster = new Monster("Oscar the Ogre", 200, 2, .6, .1, 30, 50, 30, 50);
+            case 1:     name = "Oscar the Ogre";
+                        hitPoints = 200;
+                        attackSpeed = 2;
+                        chanceToHit = .6;
+                        chanceToHeal = .1;
+                        damageMin = 30;
+                        damageMax = 50;
+                        minHeal = 30;
+                        maxHeal = 50;
                         break;
 
-            case 2:     monster = new Monster("Gnarltooth the Gremlin", 70, 5, .8, .4, 15, 30, 20, 40);
+            case 2:     name = "Gnarltooth the Gremlin";
+                        hitPoints = 70;
+                        attackSpeed = 5;
+                        chanceToHit = .8;
+                        chanceToHeal = .4;
+                        damageMin = 15;
+                        damageMax = 30;
+                        minHeal = 20;
+                        maxHeal = 40;
                         break;
 
-            case 3:     monster = new Monster("Sargath the Skeleton", 100, 3, .8, .3, 30, 50, 30, 50);
+            case 3:     name = "Sargath the Skeleton";
+                        hitPoints = 100;
+                        attackSpeed = 3;
+                        chanceToHit = .8;
+                        chanceToHeal = .3;
+                        damageMin = 30;
+                        damageMax = 50;
+                        minHeal = 30;
+                        maxHeal = 50;
                         break;
 
-            default:    monster = new Monster("Sargath the Skeleton", 100, 3, .8, .3, 30, 50, 30, 50);
+            case 4:     name = "Oswald the Orc";
+                        hitPoints = 175;
+                        attackSpeed = 3;
+                        chanceToHit = .7;
+                        chanceToHeal = .1;
+                        damageMin = 25;
+                        damageMax = 55;
+                        minHeal = 20;
+                        maxHeal = 45;
+                        break;
+
+            case 5:     name = "Darius the Dark Lord";
+                        hitPoints = 225;
+                        attackSpeed = 4;
+                        chanceToHit = .8;
+                        chanceToHeal = .3;
+                        damageMin = 45;
+                        damageMax = 60;
+                        minHeal = 35;
+                        maxHeal = 55;
+                        break;
+
+            default:    name = "Sargath the Skeleton";
+                        hitPoints = 100;
+                        attackSpeed = 3;
+                        chanceToHit = .8;
+                        chanceToHeal = .3;
+                        damageMin = 30;
+                        damageMax = 50;
+                        minHeal = 30;
+                        maxHeal = 50;
                         break;
         }//end switch
 
+        monster = new Monster(name, hitPoints, attackSpeed, chanceToHit, chanceToHeal, damageMin, damageMax, minHeal, maxHeal);
         monster.setAttack(setAbility(type));
         return monster;
     }
@@ -31,6 +89,10 @@ public class MonsterFactory {
             case 2:     return new GremlinAttack();
 
             case 3:     return new SkeletonAttack();
+
+            case 4:     return new OrcAttack();
+
+            case 5:     return new DarkLordAttack();
 
             default:    return new SkeletonAttack();
         }//end switch
